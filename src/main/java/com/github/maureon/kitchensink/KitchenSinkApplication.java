e5454c83471a59fdf9e109481fbac1b4ee4f4bfd
Licinio Fernndez Maurelo <licinio.fernandez@gmail.com>
2022-09-06 15:59:01 +0200

    Generate war and test application load entry point

--- src/main/java/com/github/maureon/kitchensink/KitchenSinkApplication.java
+++ src/main/java/com/github/maureon/kitchensink/KitchenSinkApplication.java
@@ -2,10 +2,18 @@ package com.github.maureon.kitchensink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KitchenSinkApplication {
+public class KitchenSinkApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("Bootstrapping before loading APP");
+		return application.sources(KitchenSinkApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KitchenSinkApplication.class, args);
 	}
